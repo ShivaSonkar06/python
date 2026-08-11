3e2f6730ec3e4d25e107bb2f7d6b4b88a7efc9ce
shiva_xtz <s75944289@gmail.com>
2026-08-11 13:22:18 +0530

    Add files via upload

---
+++ fundmental booster.py
@@ -0,0 +1,28 @@
+
+print("welcome to the interactive personal data collector!")
+
+input("please enter your name :")
+input("please enter your age :")
+input("please enter your height in meters :")
+input("please enter your favourite number :")
+
+print("thank you! here is the information we collected:")
+
+
+a="name: shiva"
+print(a,type(a),id(a))
+
+b="age: 22"
+print(b,type(b),id(b))
+
+c="height: 1.80"
+print(c,type(c),id(c))
+
+d="favourite numer: 69"
+print(d,type(d),id(d))
+
+a=2026
+b=22
+print("your birth year is approximately:",a-b,"(based on your age of 22)")
+
+print("thank for using the personal data collecter. goodbye!")
